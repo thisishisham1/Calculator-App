8e59aa5fac7ba7db924ef53fe41da1eb88a3a37b
hisham mohamed <hishamohmad19@gmail.com>
2023-09-13 10:32:47 +0300

    theme done

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,44 +3,57 @@ package com.example.calculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculatorapp.ui.theme.CalculatorAppTheme
+import com.example.calculatorapp.ui.theme.LightColorScheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            MaterialTheme() {
+                CalculatorAppTheme {
+                    Surface {
+                        ButtonTest()
+                    }
+
                 }
+
             }
+
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorAppTheme {
-        Greeting("Android")
+fun ButtonTest(modifier: Modifier = Modifier) {
+    Column(
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            shape = CircleShape,
+        ) {
+            Text(text = "2")
+        }
     }
 }
--- app/src/main/java/com/example/calculatorapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/calculatorapp/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.calculatorapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//Light color
+val backgroundLight = Color(0xffF1F2F3)
+val textLight = Color(0xff000000)
+val buttonHighLight = Color(0xff4B5EFC)
+val buttonMediumLight = Color(0xffD2D3DA)
+val buttonLowLight = Color(0xffFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//Dark color
+val backgroundDark = Color(0xff000000)
+val textDark = Color(0xffF1F2F3)
+val buttonHighDark = Color(0xff4B5EFC)
+val buttonMediumDark = Color(0xff4E505F)
+val buttonLowDark = Color(0xff2E2F38)
